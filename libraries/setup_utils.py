aa9e04957d7f3cbe35495601dc138d89f1767f08
Carlos Gonzalez Betancort <carlos.gonzalez@couchbase.com>
2020-09-17 11:44:14 +0100

    Add more context to setup failures

--- libraries/setup_utils.py
+++ libraries/setup_utils.py
@@ -1,8 +1,10 @@
 """This file contains usefull and complex setup functionality"""
 import subprocess
 import time
+import os
 from os.path import join
 
+import requests
 from robot.api.deco import keyword
 from robot.api import logger
 
@@ -32,3 +34,47 @@ def connect_nodes(cwd: str, node_num: int, services: str, data_size: int = 256, 
         raise subprocess.CalledProcessError(complete.returncode, complete.args)
     time.sleep(wait_for)
     logger.info('Nodes connected', also_console=True)
+
+
+@keyword(types=[str, str, str, str, int])
+def confirm_backup_service_running(host: str, log_path: str, user: str = 'Administrator', password: str = 'asdasd',
+                                   context: int = 15):
+    """confirm the backup service is running in the given host if it is not it will error out and log the last lines
+    off the backup_service log"""
+    res = requests.get(f'{host}/api/v1/config', auth=(user, password))
+    if res.status_code != 200:
+        log_end_of_backup_service_logs(log_path, context)
+        raise AssertionError(f'Backup service is not running in host {host}')
+
+
+def get_last_lines_of_log(log_path: str, context: int):
+    """Retrieve the last context lines of the backup service log"""
+    list_of_lines = []
+    with open(join(log_path, 'backup_service.log'), 'rb') as log_file:
+        log_file.seek(0, os.SEEK_END)
+        buffer = bytearray()
+        pointer_location = log_file.tell()
+
+        while pointer_location >= 0:
+            log_file.seek(pointer_location)
+            pointer_location = pointer_location - 1
+            new_byte = log_file.read(1)
+            if new_byte == b'\n':
+                list_of_lines.append(buffer.decode()[::-1])
+                if len(list_of_lines) == context:
+                    return list(reversed(list_of_lines))
+                buffer = bytearray()
+            else:
+                buffer.extend(new_byte)
+
+        # As file is read completely, if there is still data in buffer, then its first line.
+        if len(buffer) > 0:
+            list_of_lines.append(buffer.decode()[::-1])
+
+
+def log_end_of_backup_service_logs(log_path: str, context: int):
+    """Log the last lines of the abckup service log"""
+    try:
+        logger.info(get_last_lines_of_log(log_path, context))
+    except FileNotFoundError as e:
+        logger.error(f'Could not open log file, it does not exist: {e}')
--- rest_api_tests/__init__.robot
+++ rest_api_tests/__init__.robot
@@ -2,5 +2,6 @@
 Documentation    This test suite focuses on running tests against the backup service REST API
 Force Tags       rest
 Resource          ../resources/setup.resource
-Suite setup      Start cluster_run nodes and connect them    node_num=2    connect_nodes=2    services=n0:kv,n1:backup
+Suite setup      Run keywords    Start cluster_run nodes and connect them    node_num=2    connect_nodes=2    services=n0:kv,n1:backup    AND
+...              confirm backup service running    http://localhost:7101    ${NS_SERVER_PATH}${/}logs${/}n_1
 Suite teardown   Environment dependent clean up
